6da223e38b83200cbea23d3d1ce7f2b51de06404
Nikola Drcalic <nikola@Nikolas-MacBook-Pro.local>
2026-06-08 00:22:54 +0200

    Add url path

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core_cinema.views import MovieView, MovieListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movies/', MovieListView.as_view()),
+    path('movies/<int:pk>', MovieView.as_view())
 ]
